2dae36ddda9457b754b122cfa35dfb411a593a13
msanvarov <msalanvarov@gmail.com>
2020-10-07 17:25:48 -0400

    :recycle: moved header to its own component

--- src/pages/login.page.tsx
+++ src/pages/login.page.tsx
@@ -11,16 +11,20 @@ import {
   Label,
   InputGroup,
   InputGroupAddon,
+  Container,
+  Row,
+  Col,
 } from 'reactstrap';
+import globalFirebase from 'firebase';
+import { useHistory } from 'react-router-dom';
 import { useFormik } from 'formik';
 import { useTranslation } from 'react-i18next';
 import * as Yup from 'yup';
 import { useFirebase } from 'react-redux-firebase';
 import { Helmet } from 'react-helmet';
 import { Link } from 'react-router-dom';
-import LogoDarkPNG from 'assets/logo-dark.png';
-import LogoLightPNG from 'assets/logo-light.png';
-import Footer from 'components/non-auth-layout/footer/footer.component';
+import Footer from 'components/auth/footer/footer.component';
+import Header from 'components/auth/header/header.component';
 
 const LoginSchema = Yup.object().shape({
   email: Yup.string().email('Invalid email').required('Required'),
@@ -29,20 +33,36 @@ const LoginSchema = Yup.object().shape({
 
 const LoginPage = () => {
   const firebase = useFirebase();
+  const history = useHistory();
   const { t } = useTranslation();
   const [formErrors, setFormErrors] = useState<string>();
-  const { handleChange, handleBlur, handleSubmit, errors, touched, values } = useFormik({
+  const {
+    handleChange,
+    handleBlur,
+    handleSubmit,
+    errors,
+    touched,
+    values,
+    setFieldValue,
+  } = useFormik({
     validationSchema: LoginSchema,
-    initialValues: { email: '', password: '' },
-    onSubmit: async ({ email, password }) => {
+    initialValues: { email: '', password: '', remember: true },
+    onSubmit: async ({ email, password, remember }) => {
       try {
+        // checkbox persistance preference
+        if (remember) {
+          await firebase.auth().setPersistence(globalFirebase.auth.Auth.Persistence.LOCAL);
+        } else {
+          await firebase.auth().setPersistence(globalFirebase.auth.Auth.Persistence.SESSION);
+        }
         // login
         const { user } = await firebase.login({ email, password });
         if (!user?.emailVerified) {
           setFormErrors(
-            'Please verify your account by clicking the Hopin registration link in the email',
+            'Please verify your account by clicking the Chatr registration link in the email',
           );
         }
+        history.push('/dashboard');
       } catch (error) {
         setFormErrors((error as Error).message);
       }
@@ -50,96 +70,106 @@ const LoginPage = () => {
   });
   return (
     <>
-      <Helmet title="Login" />
+      <Helmet title="Login | Chatr - Messaging Platform" />
+      <div className="account-pages my-5 pt-sm-5">
+        <Container>
+          <Row className="justify-content-center">
+            <Col md={8} lg={6} xl={5}>
+              <Header heading={t('Sign in')} subHeading={t('Sign in to continue to Chatr.')} />
 
-      <div className="text-center mb-4">
-        <Link to="/" className="auth-logo mb-5 d-block">
-          <img src={LogoDarkPNG} alt="dark logo" height="120" className="logo logo-dark" />
-          <img src={LogoLightPNG} alt="light logo" height="120" className="logo logo-light" />
-        </Link>
+              <Card>
+                <CardBody className="p-4">
+                  {formErrors && <Alert color="danger">{formErrors}</Alert>}
+                  <div className="p-3">
+                    <Form onSubmit={handleSubmit}>
+                      <FormGroup>
+                        <Label>{t('Email')}</Label>
+                        <InputGroup className="mb-3 bg-soft-light input-group-lg rounded-lg">
+                          <InputGroupAddon addonType="prepend">
+                            <span className="input-group-text border-light text-muted">
+                              <i className="ri-user-2-line"></i>
+                            </span>
+                          </InputGroupAddon>
+                          <Input
+                            type="text"
+                            id="email"
+                            name="email"
+                            className="form-control bg-soft-light border-light"
+                            placeholder="Enter email"
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                            value={values.email}
+                            invalid={touched.email && errors.email ? true : false}
+                          />
+                          {touched.email && errors.email ? (
+                            <FormFeedback type="invalid">{errors.email}</FormFeedback>
+                          ) : null}
+                        </InputGroup>
+                      </FormGroup>
 
-        <h4>{t('Sign in')}</h4>
-        <p className="text-muted mb-4">{t('Sign in to continue to Chatr.')}</p>
+                      <FormGroup className="mb-4">
+                        <div className="float-right">
+                          <Link to="/forgot-password" className="text-muted font-size-13">
+                            {t('Forgot password')}?
+                          </Link>
+                        </div>
+                        <Label>{t('Password')}</Label>
+                        <InputGroup className="mb-3 bg-soft-light input-group-lg rounded-lg">
+                          <InputGroupAddon addonType="prepend">
+                            <span className="input-group-text border-light text-muted">
+                              <i className="ri-lock-2-line"></i>
+                            </span>
+                          </InputGroupAddon>
+                          <Input
+                            type="password"
+                            id="password"
+                            name="password"
+                            className="form-control bg-soft-light border-light"
+                            placeholder="Enter Password"
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                            value={values.password}
+                            invalid={touched.password && errors.password ? true : false}
+                          />
+                          {touched.password && errors.password ? (
+                            <FormFeedback type="invalid">{errors.password}</FormFeedback>
+                          ) : null}
+                        </InputGroup>
+                      </FormGroup>
+
+                      <div className="custom-control custom-checkbox mb-4">
+                        <Input
+                          type="checkbox"
+                          checked={values.remember}
+                          onChange={() => setFieldValue('remember', !values.remember)}
+                          className="custom-control-input"
+                          id="remember-check"
+                        />
+                        <Label className="custom-control-label" htmlFor="remember-check">
+                          {t('Remember me')}
+                        </Label>
+                      </div>
+
+                      <div>
+                        <Button
+                          color="primary"
+                          block
+                          className="waves-effect waves-light"
+                          type="submit"
+                        >
+                          {t('Sign in')}
+                        </Button>
+                      </div>
+                    </Form>
+                  </div>
+                </CardBody>
+              </Card>
+
+              <Footer text="Don't have an account" linkText="Signup now" linkTo="/register" />
+            </Col>
+          </Row>
+        </Container>
       </div>
-
-      <Card>
-        <CardBody className="p-4">
-          {formErrors && <Alert color="danger">{formErrors}</Alert>}
-          <div className="p-3">
-            <Form onSubmit={handleSubmit}>
-              <FormGroup>
-                <Label>{t('Email')}</Label>
-                <InputGroup className="mb-3 bg-soft-light input-group-lg rounded-lg">
-                  <InputGroupAddon addonType="prepend">
-                    <span className="input-group-text border-light text-muted">
-                      <i className="ri-user-2-line"></i>
-                    </span>
-                  </InputGroupAddon>
-                  <Input
-                    type="text"
-                    id="email"
-                    name="email"
-                    className="form-control bg-soft-light border-light"
-                    placeholder="Enter email"
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    value={values.email}
-                    invalid={touched.email && errors.email ? true : false}
-                  />
-                  {touched.email && errors.email ? (
-                    <FormFeedback type="invalid">{errors.email}</FormFeedback>
-                  ) : null}
-                </InputGroup>
-              </FormGroup>
-
-              <FormGroup className="mb-4">
-                <div className="float-right">
-                  <Link to="forget-password" className="text-muted font-size-13">
-                    {t('Forgot password')}?
-                  </Link>
-                </div>
-                <Label>{t('Password')}</Label>
-                <InputGroup className="mb-3 bg-soft-light input-group-lg rounded-lg">
-                  <InputGroupAddon addonType="prepend">
-                    <span className="input-group-text border-light text-muted">
-                      <i className="ri-lock-2-line"></i>
-                    </span>
-                  </InputGroupAddon>
-                  <Input
-                    type="password"
-                    id="password"
-                    name="password"
-                    className="form-control bg-soft-light border-light"
-                    placeholder="Enter Password"
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    value={values.password}
-                    invalid={touched.password && errors.password ? true : false}
-                  />
-                  {touched.password && errors.password ? (
-                    <FormFeedback type="invalid">{errors.password}</FormFeedback>
-                  ) : null}
-                </InputGroup>
-              </FormGroup>
-
-              <div className="custom-control custom-checkbox mb-4">
-                <Input type="checkbox" className="custom-control-input" id="remember-check" />
-                <Label className="custom-control-label" htmlFor="remember-check">
-                  {t('Remember me')}
-                </Label>
-              </div>
-
-              <div>
-                <Button color="primary" block className=" waves-effect waves-light" type="submit">
-                  {t('Sign in')}
-                </Button>
-              </div>
-            </Form>
-          </div>
-        </CardBody>
-      </Card>
-
-      <Footer text="Don't have an account" linkText="Signup now" linkTo="/register" />
     </>
   );
 };
--- src/pages/register.page.tsx
+++ src/pages/register.page.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-
+import Gravatar from 'gravatar';
 import { useFormik } from 'formik';
 import * as Yup from 'yup';
 import {
@@ -15,16 +15,21 @@ import {
   Label,
   InputGroup,
   InputGroupAddon,
+  Container,
+  Row,
+  Col,
 } from 'reactstrap';
+import { useFirebase } from 'react-redux-firebase';
 import { useTranslation } from 'react-i18next';
-import LogoDarkPNG from 'assets/logo-dark.png';
-import LogoLightPNG from 'assets/logo-light.png';
 import { Helmet } from 'react-helmet';
-import Footer from 'components/non-auth-layout/footer/footer.component';
+import Footer from 'components/auth/footer/footer.component';
+import Header from 'components/auth/header/header.component';
 
 const RegisterPage = () => {
+  const firebase = useFirebase();
   const { t } = useTranslation();
-  const [formError] = useState<string>();
+  const [formError, setFormError] = useState<string>();
+  const [finishedRegistration, setFinishedRegistration] = useState<boolean>(false);
   // validation
   const { handleChange, handleBlur, handleSubmit, errors, touched, values } = useFormik({
     initialValues: {
@@ -37,126 +42,157 @@ const RegisterPage = () => {
       email: Yup.string().email('Enter proper email').required('Required'),
       password: Yup.string().required('Required'),
     }),
-    onSubmit: ({ username, email, password }) => {
-      console.log(username, email, password);
+    onSubmit: async ({ username, email, password }) => {
+      try {
+        await firebase.createUser(
+          { email, password },
+          {
+            email,
+            username,
+          },
+        );
+
+        // firebase set profile settings
+        firebase.updateAuth(
+          {
+            displayName: username,
+            photoURL: Gravatar.url(email, {
+              protocol: 'https',
+              s: '100',
+              r: 'pg',
+            }),
+          },
+          true,
+        );
+        setFinishedRegistration(true);
+      } catch (error) {
+        setFormError((error as Error).message);
+      }
     },
   });
 
   return (
     <>
-      <Helmet title="Register" />
+      <Helmet title="Register | Chatr - Messaging Platform" />
 
-      <div className="text-center mb-4">
-        <Link to="/" className="auth-logo mb-5 d-block">
-          <img src={LogoDarkPNG} alt="dark logo" height="120" className="logo logo-dark" />
-          <img src={LogoLightPNG} alt="light logo" height="120" className="logo logo-light" />
-        </Link>
+      <div className="account-pages my-5 pt-sm-5">
+        <Container>
+          <Row className="justify-content-center">
+            <Col md={8} lg={6} xl={5}>
+              <Header heading={t('Sign up')} subHeading={`${t('Get your Chatr account now')}.`} />
 
-        <h4>{t('Sign up')}</h4>
-        <p className="text-muted mb-4">{t('Get your Chatr account now')}.</p>
+              <Card>
+                <CardBody className="p-4">
+                  {formError && <Alert color="danger">{formError}</Alert>}
+                  {finishedRegistration && (
+                    <Alert variant="success">Thank You for registering with us!</Alert>
+                  )}
+                  <div className="p-3">
+                    <Form onSubmit={handleSubmit}>
+                      <FormGroup>
+                        <Label>{t('Email')}</Label>
+                        <InputGroup className="mb-3 bg-soft-light input-group-lg rounded-lg">
+                          <InputGroupAddon addonType="prepend">
+                            <span className="input-group-text border-light text-muted">
+                              <i className="ri-mail-line"></i>
+                            </span>
+                          </InputGroupAddon>
+                          <Input
+                            type="text"
+                            id="email"
+                            name="email"
+                            className="form-control bg-soft-light border-light"
+                            placeholder="Enter email"
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                            value={values.email}
+                            invalid={touched.email && errors.email ? true : false}
+                          />
+                          {touched.email && errors.email ? (
+                            <FormFeedback type="invalid">{errors.email}</FormFeedback>
+                          ) : null}
+                        </InputGroup>
+                      </FormGroup>
+
+                      <FormGroup>
+                        <Label>{t('Username')}</Label>
+                        <InputGroup className="mb-3 bg-soft-light input-group-lg rounded-lg">
+                          <InputGroupAddon addonType="prepend">
+                            <span className="input-group-text border-light text-muted">
+                              <i className="ri-user-2-line"></i>
+                            </span>
+                          </InputGroupAddon>
+                          <Input
+                            type="text"
+                            id="username"
+                            name="username"
+                            className="form-control bg-soft-light border-light"
+                            placeholder="Enter Username"
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                            value={values.username}
+                            invalid={touched.username && errors.username ? true : false}
+                          />
+                          {touched.username && errors.username ? (
+                            <FormFeedback type="invalid">{errors.username}</FormFeedback>
+                          ) : null}
+                        </InputGroup>
+                      </FormGroup>
+
+                      <FormGroup className="mb-4">
+                        <Label>{t('Password')}</Label>
+                        <InputGroup className="mb-3 bg-soft-light input-group-lg rounded-lg">
+                          <InputGroupAddon addonType="prepend">
+                            <span className="input-group-text border-light text-muted">
+                              <i className="ri-lock-2-line"></i>
+                            </span>
+                          </InputGroupAddon>
+                          <Input
+                            type="password"
+                            id="password"
+                            name="password"
+                            className="form-control bg-soft-light border-light"
+                            placeholder="Enter Password"
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                            value={values.password}
+                            invalid={touched.password && errors.password ? true : false}
+                          />
+                          {touched.password && errors.password ? (
+                            <FormFeedback type="invalid">{errors.password}</FormFeedback>
+                          ) : null}
+                        </InputGroup>
+                      </FormGroup>
+
+                      <div>
+                        <Button
+                          color="primary"
+                          block
+                          className=" waves-effect waves-light"
+                          type="submit"
+                        >
+                          Sign up
+                        </Button>
+                      </div>
+
+                      <div className="mt-4 text-center">
+                        <p className="text-muted mb-0">
+                          {t('By registering you agree to the Chatr')}{' '}
+                          <Link to="#" className="text-primary">
+                            {t('Terms of Use')}
+                          </Link>
+                        </p>
+                      </div>
+                    </Form>
+                  </div>
+                </CardBody>
+              </Card>
+
+              <Footer text="Already have an account" linkText="Signin" linkTo="/login" />
+            </Col>
+          </Row>
+        </Container>
       </div>
-
-      <Card>
-        <CardBody className="p-4">
-          {formError && <Alert color="danger">{formError}</Alert>}
-          {false && <Alert variant="success">Thank You for registering with us!</Alert>}
-          <div className="p-3">
-            <Form onSubmit={handleSubmit}>
-              <FormGroup>
-                <Label>{t('Email')}</Label>
-                <InputGroup className="mb-3 bg-soft-light input-group-lg rounded-lg">
-                  <InputGroupAddon addonType="prepend">
-                    <span className="input-group-text border-light text-muted">
-                      <i className="ri-mail-line"></i>
-                    </span>
-                  </InputGroupAddon>
-                  <Input
-                    type="text"
-                    id="email"
-                    name="email"
-                    className="form-control bg-soft-light border-light"
-                    placeholder="Enter email"
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    value={values.email}
-                    invalid={touched.email && errors.email ? true : false}
-                  />
-                  {touched.email && errors.email ? (
-                    <FormFeedback type="invalid">{errors.email}</FormFeedback>
-                  ) : null}
-                </InputGroup>
-              </FormGroup>
-
-              <FormGroup>
-                <Label>{t('Username')}</Label>
-                <InputGroup className="mb-3 bg-soft-light input-group-lg rounded-lg">
-                  <InputGroupAddon addonType="prepend">
-                    <span className="input-group-text border-light text-muted">
-                      <i className="ri-user-2-line"></i>
-                    </span>
-                  </InputGroupAddon>
-                  <Input
-                    type="text"
-                    id="username"
-                    name="username"
-                    className="form-control bg-soft-light border-light"
-                    placeholder="Enter Username"
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    value={values.username}
-                    invalid={touched.username && errors.username ? true : false}
-                  />
-                  {touched.username && errors.username ? (
-                    <FormFeedback type="invalid">{errors.username}</FormFeedback>
-                  ) : null}
-                </InputGroup>
-              </FormGroup>
-
-              <FormGroup className="mb-4">
-                <Label>{t('Password')}</Label>
-                <InputGroup className="mb-3 bg-soft-light input-group-lg rounded-lg">
-                  <InputGroupAddon addonType="prepend">
-                    <span className="input-group-text border-light text-muted">
-                      <i className="ri-lock-2-line"></i>
-                    </span>
-                  </InputGroupAddon>
-                  <Input
-                    type="password"
-                    id="password"
-                    name="password"
-                    className="form-control bg-soft-light border-light"
-                    placeholder="Enter Password"
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    value={values.password}
-                    invalid={touched.password && errors.password ? true : false}
-                  />
-                  {touched.password && errors.password ? (
-                    <FormFeedback type="invalid">{errors.password}</FormFeedback>
-                  ) : null}
-                </InputGroup>
-              </FormGroup>
-
-              <div>
-                <Button color="primary" block className=" waves-effect waves-light" type="submit">
-                  Sign up
-                </Button>
-              </div>
-
-              <div className="mt-4 text-center">
-                <p className="text-muted mb-0">
-                  {t('By registering you agree to the Chatr')}{' '}
-                  <Link to="#" className="text-primary">
-                    {t('Terms of Use')}
-                  </Link>
-                </p>
-              </div>
-            </Form>
-          </div>
-        </CardBody>
-      </Card>
-
-      <Footer text="Already have an account" linkText="Signin" linkTo="/login" />
     </>
   );
 };
