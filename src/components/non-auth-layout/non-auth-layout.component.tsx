8c718190a5175832a250e6a29e2d5b94281aa6cc
msanvarov <msalanvarov@gmail.com>
2020-10-07 02:52:02 -0400

    :recycle: removing redundant code from pages

--- src/components/non-auth-layout/non-auth-layout.component.tsx
+++ src/components/non-auth-layout/non-auth-layout.component.tsx
@@ -1,19 +1,22 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
-import { useLocation } from 'react-router';
+import { Col, Container, Row } from 'reactstrap';
 
 import './non-auth-layout.styles.scss';
 
 const NonAuthLayout: React.FC = ({ children }) => {
-  const location = useLocation();
   return (
     <>
-      <Helmet
-        title={`${
-          location.pathname.charAt(1).toUpperCase + location.pathname.slice(2)
-        } | Chatr - Messaging Platform`}
-      />
-      {children}
+      <Helmet title="Chatr - Messaging Platform" />
+      <div className="account-pages my-5 pt-sm-5">
+        <Container>
+          <Row className="justify-content-center">
+            <Col md={8} lg={6} xl={5}>
+              {children}
+            </Col>
+          </Row>
+        </Container>
+      </div>
     </>
   );
 };
